3c344a114da1fd4729b6d7dc5f0e3cd7ca93c7ae
Verónica Guzmán Ochoa <heylistennavi@proton.me>
2026-03-22 21:23:59 -0700

    fix: wrongly named buusinesses relationship in category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -18,7 +18,7 @@ class Category extends Model
         'is_active' => 'boolean',
     ];
 
-    public function restaurants()
+    public function businesses()
     {
         return $this->hasMany(Business::class, "category_id");
     }
